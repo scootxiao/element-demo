e0100e40ae1b7bc1a9a66c76f7daff8d7db38351
xiaogang <1321044565@qq.com>
2017-10-10 20:17:52 +0800

    例子查看到 Input 输入框

--- src/App.vue
+++ src/App.vue
@@ -31,13 +31,218 @@
     <i class="el-icon-share"></i>
     <i class="el-icon-delete"></i>
     <el-button type="primary" icon="search">搜索</el-button>
+
+    <div class="block">
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+    </div>
+
+    <div class="block">
+      <el-button :plain="true" type="success">成功按钮</el-button>
+      <el-button :plain="true" type="warning">警告按钮</el-button>
+      <el-button :plain="true" type="danger">危险按钮</el-button>
+      <el-button :plain="true" type="info">信息按钮</el-button>
+    </div>
+
+    <div class="block">
+      <el-button type="primary" icon="edit"></el-button>
+      <el-button type="primary" icon="share"></el-button>
+      <el-button type="primary" icon="delete"></el-button>
+      <el-button type="primary" icon="search">搜索</el-button>
+      <el-button type="primary">上传<i class="el-icon-upload el-icon--right"></i></el-button>
+    </div>
+
+    <div class="block">
+      <el-button-group>
+        <el-button type="primary" icon="arrow-left">上一页</el-button>
+        <el-button type="primary">下一页<i class="el-icon-arrow-right el-icon--right"></i></el-button>
+      </el-button-group>
+      <el-button-group>
+        <el-button type="primary" icon="edit"></el-button>
+        <el-button type="primary" icon="share"></el-button>
+        <el-button type="primary" icon="delete"></el-button>
+      </el-button-group>
+    </div>
+    
+    <div class="block">
+      <el-button type="primary" :loading="true">加载中</el-button>
+      <el-button type="primary" size="large">大型按钮</el-button>
+      <el-button type="primary">正常按钮</el-button>
+      <el-button type="primary" size="small">小型按钮</el-button>
+      <el-button type="primary" size="mini">超小按钮</el-button>
+    </div>
+
+
+    <el-radio class="radio" v-model="radio" label="1">备选项</el-radio>
+    <el-radio class="radio" v-model="radio" label="2">备选项</el-radio>
+
+    <el-radio-group v-model="radio2">
+      <el-radio :label="3">备选项</el-radio>
+      <el-radio :label="6">备选项</el-radio>
+      <el-radio :label="9">备选项</el-radio>
+    </el-radio-group>
+
+    <div class="block">
+     <el-radio-group v-model="radio3">
+        <el-radio-button label="上海"></el-radio-button>
+        <el-radio-button label="北京"></el-radio-button>
+        <el-radio-button label="广州"></el-radio-button>
+        <el-radio-button label="深圳"></el-radio-button>
+      </el-radio-group>
+      <div style="margin: 15px 0;"></div>
+      <el-radio-group v-model="radio4">
+        <el-radio-button label="上海"></el-radio-button>
+        <el-radio-button label="北京" :disabled="true"></el-radio-button>
+        <el-radio-button label="广州"></el-radio-button>
+        <el-radio-button label="深圳"></el-radio-button>
+      </el-radio-group>
+      <div style="margin: 15px 0;"></div>
+      <el-radio-group v-model="radio5" :disabled="true">
+        <el-radio-button label="上海"></el-radio-button>
+        <el-radio-button label="北京"></el-radio-button>
+        <el-radio-button label="广州"></el-radio-button>
+        <el-radio-button label="深圳"></el-radio-button>
+      </el-radio-group>
+
+      <div class="block">
+        <el-checkbox :indeterminate="isIndeterminate" v-model="checkAll" @change="handleCheckAllChange">全选</el-checkbox>
+        <el-checkbox-group v-model="checkedCities" @change="handleCheckedCitiesChange">
+          <el-checkbox v-for="city in cities" :label="city" :key="city">{{city}}</el-checkbox>
+        </el-checkbox-group>
+      </div>
+
+      <div class="block">
+        <el-checkbox-group 
+          v-model="checkedCities1"
+          :min="1"
+          :max="2">
+          <el-checkbox v-for="city in cities" :label="city" :key="city">{{city}}</el-checkbox>
+        </el-checkbox-group>
+      </div>
+
+    </div>
+
+    <div class="block">
+      <el-checkbox-group v-model="checkboxGroup1">
+        <el-checkbox-button v-for="city in cities" :label="city" :key="city">{{city}}</el-checkbox-button>
+      </el-checkbox-group>
+      <div style="margin: 15px 0;"></div>
+      <el-checkbox-group v-model="checkboxGroup2" size="small">
+        <el-checkbox-button v-for="city in cities" :label="city" :disabled="city === '深圳'" :key="city">{{city}}</el-checkbox-button>
+      </el-checkbox-group>
+      <div style="margin: 15px 0;"></div>
+      <el-checkbox-group v-model="checkboxGroup3" size="large" fill="#ff0" text-color="#0f0" :min="1" :max="3">
+        <el-checkbox-button v-for="city in cities" :label="city" :key="city">{{city}}</el-checkbox-button>
+      </el-checkbox-group>
+    </div>
+
+    <div class="block">
+      <el-input v-model="input" placeholder="请输入内容"></el-input>
+      <div style="margin: 15px 0;"></div>
+      <el-input
+        placeholder="请选择日期"
+        icon="search"
+        v-model="input2"
+        :on-icon-click="handleIconClick">
+      </el-input>
+    </div>
+
+    <div class='block'>
+      <el-input
+        type="textarea"
+        :rows="2"
+        placeholder="请输入内容"
+        v-model="textarea">
+      </el-input>
+
+      <el-input
+        type="textarea"
+        autosize
+        placeholder="请输入内容"
+        v-model="textarea2">
+      </el-input>
+      <div style="margin: 20px 0;"></div>
+      <el-input
+        type="textarea"
+        :autosize="{ minRows: 2, maxRows: 4}"
+        placeholder="请输入内容"
+        v-model="textarea3">
+      </el-input>
+    </div>
+
+    <div class='block'>
+      <div>
+        <el-input placeholder="请输入内容" v-model="input3">
+          <template slot="prepend">Http://</template>
+        </el-input>
+      </div>
+      <div style="margin-top: 15px;">
+        <el-input placeholder="请输入内容" v-model="input4">
+          <template slot="append">.com</template>
+        </el-input>
+      </div>
+      <div style="margin-top: 15px;">
+        <el-input placeholder="请输入内容" v-model="input5">
+          <el-select v-model="select" slot="prepend" placeholder="请选择">
+            <el-option label="餐厅名" value="1"></el-option>
+            <el-option label="订单号" value="2"></el-option>
+            <el-option label="用户电话" value="3"></el-option>
+          </el-select>
+          <el-button slot="append" icon="search"></el-button>
+        </el-input>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  const cityOptions = ['上海', '北京', '广州', '深圳'];
+  export default {
+    name: 'app',
+    data () {
+      return {
+        radio: '1',
+        radio2: 3,
+        radio3: '上海',
+        radio4: '上海',
+        radio5: '上海',
+        checkAll: true,
+        checkedCities: ['上海', '北京'],
+        cities: cityOptions,
+        isIndeterminate: true,
+        checkedCities1: ['上海', '北京'],
+        checkboxGroup1: ['上海'],
+        checkboxGroup2: ['北京'],
+        checkboxGroup3: ['广州'],
+        input: 'this is the first!!!',
+        input2: '',
+        textarea: '',
+        textarea2: '',
+        textarea3: '',
+        input3: '',
+        input4: '',
+        input5: '',
+        select: ''
+      };
+    },
+    methods: {
+      handleCheckAllChange(event) {
+        this.checkedCities = event.target.checked ? cityOptions : [];
+        this.isIndeterminate = false;
+      },
+      handleCheckedCitiesChange(value) {
+        let checkedCount = value.length;
+        this.checkAll = checkedCount === this.cities.length;
+        this.isIndeterminate = checkedCount > 0 && checkedCount < this.cities.length;
+      },
+      handleIconClick(ev) {
+        console.log(ev);
+      }
+    }
+  }
 </script>
 
 <style>
@@ -66,5 +271,10 @@ export default {
   .row-bg {
     padding: 10px 0;
     background-color: #f9fafc;
+  }
+
+  .block {
+    padding: 30px 24px;
+    overflow: hidden;
   }
 </style>
